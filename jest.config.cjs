53ba8c059eefc1ecec059c5e502fe84553ab10f7
florsalvador <florsalvador219@gmail.com>
2024-06-24 12:09:55 -0500

    Modify config files to mock svg imports

---
+++ jest.config.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+
+module.exports = {
+  collectCoverage: true,
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
+  },
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+  ],
+  moduleNameMapper: {
+    "^.+\\.module\\.css$": "identity-obj-proxy",
+    "^.+\\.(css|png|jpg|jpeg|svg)$": "<rootDir>/test/__mocks__/fileMock.cjs",
+  },
+};
