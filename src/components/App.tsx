dc510e29c0244c31e4fac939c7a523bcd5a2aacb
florsalvador <florsalvador219@gmail.com>
2024-06-27 17:48:52 -0500

    feat: Add h1 and MovieCard component in App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import '../styles/App.css'
+// import { useState } from "react"
+import "../styles/App.css"
+import MovieCard from "./MovieCard"
+// import getMovies from "../services/APIService.ts"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  // const movieList = getMovies();
+  const movieTest = {
+    "id": 653346,
+    "title": "Kingdom of the Planet of the Apes",
+    // "title": undefined,
+    "year": "2024",
+    // "year": null,
+    "posterPath": "https://image.tmdb.org/t/p/w500/gKkl37BQuKTanygYQG1pyYgLVgf.jpg",
+    // "posterPath": "https://image.tmdb.org/t/p/w500null",
+    "genreIds": [ 878, 12, 28 ],
+    "overview": "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike."
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Bestmovies</h1>
+      <MovieCard movie={movieTest} />
     </>
   )
 }
