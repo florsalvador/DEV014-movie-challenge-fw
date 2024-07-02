25db381212b8a178512dd804279c4ed16d7fc18e
florsalvador <florsalvador219@gmail.com>
2024-07-01 22:06:18 -0500

    feat: Integrate Pagination component in Home.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Home";
-// import Pagination from './Pagination';
 import "../styles/App.css";
 
 function App() {
@@ -8,9 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/" element={<Pagination currentPage={1} totalPages={10} onSelectPage={function (n: number): void {
-          console.log("You clicked page " + n);
-        } } />} /> */}
       </Routes>
     </Router>
   )
