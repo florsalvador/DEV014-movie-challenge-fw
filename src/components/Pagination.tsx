6813e00c296ddedd5ebf2bbadbecbc13e201a3c0
florsalvador <florsalvador219@gmail.com>
2024-07-01 22:07:38 -0500

    feat: Improve Pagination component so it does not show all the pages

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -7,20 +7,34 @@ interface PaginationProps {
 }
 
 function Pagination({currentPage, totalPages, onSelectPage}: PaginationProps) {
+  const buttonBefore = <button key={"before"} className="btn-page" onClick={() => onSelectPage(currentPage-1)} disabled={currentPage == 1 ? true : false}>{"<"}</button>
+  const buttonLastPage = <button key={"last-page"} className="btn-page" onClick={() => onSelectPage(totalPages)}>{totalPages}</button>
+  const buttonAfter = <button key={"after"} className="btn-page" onClick={() => onSelectPage(currentPage+1)} disabled={currentPage == totalPages ? true : false}>{">"}</button>
+  
   const buttons = [];
-  buttons.push(<button key={"before"} className="btn-page" onClick={() => onSelectPage(currentPage-1)} disabled={currentPage == 1 ? true : false}>{"<"}</button>);
-  for (let i = 1; i <= totalPages; i++) {
+  const rangeInitial = currentPage <= 2 ? 1 : currentPage - 2;
+  const rangeLast = currentPage <= 1 ? 3 : currentPage;
+  for (let i = rangeInitial; i <= rangeLast + 2; i++) {
     if (i == currentPage) { 
       buttons.push(<button key={i} className="btn-current-page" onClick={() => onSelectPage(i)}>{i}</button>);
-    } else {
+    } else if (i < totalPages) {
       buttons.push(<button key={i} className="btn-page" onClick={() => onSelectPage(i)}>{i}</button>);
     }
   }
-  buttons.push(<button key={"after"} className="btn-page" onClick={() => onSelectPage(currentPage+1)} disabled={currentPage == totalPages ? true : false}>{">"}</button>);
+
+  const moreBefore = <button key={"more-before"} className="btn-before-after" disabled={true}>{". . ."}</button>
+  const moreAfter = <button key={"more-after"} className="btn-before-after" disabled={true}>{". . ."}</button>
 
   return (
     <>
-      {buttons}
+      <div className="pagination">
+        {buttonBefore}
+        {rangeInitial !== 1 && moreBefore}
+        {buttons}
+        {rangeLast < totalPages - 2 && moreAfter}
+        {buttonLastPage}
+        {buttonAfter}
+      </div>
     </>
   )
 }
