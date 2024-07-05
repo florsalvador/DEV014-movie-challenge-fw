b9643994ab11e397451c05899a61d9e7f39ee9f4
florsalvador <florsalvador219@gmail.com>
2024-07-05 14:44:22 -0500

    feat: Create ListOptions component

---
+++ src/components/ListOptions .tsx
@@ -0,0 +1,28 @@
+import "../styles/ListOptions.css";
+
+interface ListOptionsProps {
+  selectType: string,
+  options: { value: string, label: string }[],
+  selectedOption: {value: string, label: string} | null,
+  onChange(e: string): void,
+  onClear(e?: string): void
+}
+
+function ListOptions({selectType, options, selectedOption, onChange, onClear}: ListOptionsProps) {
+  const optionElements = options.map(option => 
+    <option key={option.value} value={option.value}>{option.label}</option>
+  );
+  return (
+    <>
+      <form>
+        <select name="filter" id="filter-by-genre" defaultValue={selectedOption?.value} onChange={e => onChange(e.target.value)}>
+          <option value="">{selectType}</option>
+          {optionElements}
+        </select>
+        <button type="reset" onClick={() => onClear()}>Clear</button>
+      </form>
+    </>
+  )
+}
+
+export default ListOptions
