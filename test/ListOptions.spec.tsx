cd3e2e083fa417d9ee68792d3ef508ad2ea7e305
florsalvador <florsalvador219@gmail.com>
2024-07-07 18:56:56 -0500

    test: Add tests for ListOptions component

--- test/ListOptions.spec.tsx
+++ test/ListOptions.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import ListOptions from "../src/components/ListOptions";
 
 describe("ListOptions component", () => {
@@ -19,14 +19,41 @@ describe("ListOptions component", () => {
       onClear={onClick}  
     />);
     const select = screen.getByTestId("select")
-    const optionNoValue = screen.getByText("Filter by genre")
     const option1 = screen.getByTestId(options[0].value)
     const option2 = screen.getByTestId(options[1].value)
     const button = screen.getByRole("button")
     expect(select).toBeInTheDocument();
-    expect(optionNoValue).toBeInTheDocument();
     expect(option1).toBeInTheDocument();
     expect(option2).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+
+  test("Changes the select value when an option is selected", () => {
+    render(<ListOptions 
+      selectType={"Filter by genre"} 
+      options={options} 
+      selectedOption={null} 
+      onChange={onClick} 
+      onClear={onClick}  
+    />);
+    const select = screen.getByTestId("select")
+    fireEvent.change((select), { target: { value: options[0].value } });
+    expect(select).toHaveValue(options[0].value);
+  });
+
+  test("Resets select value when the button is clicked", () => {
+    render(<ListOptions 
+      selectType={"Filter by genre"} 
+      options={options} 
+      selectedOption={options[1]} 
+      onChange={onClick} 
+      onClear={onClick}  
+    />);
+    const select = screen.getByTestId("select");
+    const button = screen.getByRole("button");
+    fireEvent.click((button));
+    waitFor(() => {
+      expect(select).toHaveValue("");
+    });
   });
 })
