6c0a9da6cf36ab5f2896a8806947752b2cff6146
florsalvador <florsalvador219@gmail.com>
2024-07-10 14:14:50 -0500

    test: Fix mock data and add another test for MovieCard component

--- test/MovieCard.spec.tsx
+++ test/MovieCard.spec.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import MovieCard from "../src/components/MovieCard.tsx";
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+   useNavigate: () => mockedUsedNavigate,
+}));
 
 describe("MovieCard component", () => {
   const movie = {
@@ -10,29 +17,39 @@ describe("MovieCard component", () => {
     year: "2024",
     posterPath: "https://image.tmdb.org/t/p/w500/gKkl37BQuKTanygYQG1pyYgLVgf.jpg",
     genres: [ "Science Fiction", "Adventure", "Action" ],
-    overview: "Several generations in the future following Caesar's reign, apes are now the dominant species."
+    overview: "Several generations in the future following Caesar's reign, apes are now the dominant species.",
+    voteAverage: 6.879,
+    voteCount: 1150,
   }
 
   test("Renders movie title", () => {
-    render(<MovieCard movie={movie} />);
+    render(<MemoryRouter>
+      <MovieCard movie={movie} />
+    </MemoryRouter>);
     const title = screen.getByText(movie.title);
     expect(title).toBeInTheDocument();
   });
 
   test("Renders movie year", () => {
-    render(<MovieCard movie={movie} />);
+    render(<MemoryRouter>
+      <MovieCard movie={movie} />
+    </MemoryRouter>);
     const year = screen.getByText(/2024/);
     expect(year).toBeInTheDocument();
   });
 
   test("Renders movie genres", () => {
-    render(<MovieCard movie={movie} />);
+    render(<MemoryRouter>
+      <MovieCard movie={movie} />
+    </MemoryRouter>);
     const genres = screen.getByText(/Science Fiction, Adventure/);
     expect(genres).toBeInTheDocument();
   });
 
   test("Renders image with correct src and alt", () => {
-    render(<MovieCard movie={movie} />);
+    render(<MemoryRouter>
+      <MovieCard movie={movie} />
+    </MemoryRouter>);
     const image = screen.getByRole("img");
     expect(image).toBeInTheDocument();
     expect(image).toHaveAttribute("src", movie.posterPath);
@@ -46,9 +63,13 @@ describe("MovieCard component", () => {
       year: "2023",
       posterPath: "https://image.tmdb.org/t/p/w500/8htJ7keZTwa08aC9OKyiqaq1cNJ.jpg",
       genres: [ "War" ],
-      overview: "A young pilot, fresh out of training, is called to join a Flight while they wait for the call to scramble."
+      overview: "A young pilot, fresh out of training, is called to join a Flight while they wait for the call to scramble.",
+      voteAverage: 7.412,
+      voteCount: 51,
     }
-    render(<MovieCard movie={anotherMovie} />);
+    render(<MemoryRouter>
+      <MovieCard movie={anotherMovie} />
+    </MemoryRouter>);
     const title = screen.getByText(anotherMovie.title);
     const year = screen.getByText(/2023/);
     expect(title).toBeInTheDocument();
@@ -62,9 +83,13 @@ describe("MovieCard component", () => {
       year: "2023",
       posterPath: "https://image.tmdb.org/t/p/w500/8htJ7keZTwa08aC9OKyiqaq1cNJ.jpg",
       genres: [ "War" ],
-      overview: "A young pilot, fresh out of training, is called to join a Flight while they wait for the call to scramble."
+      overview: "A young pilot, fresh out of training, is called to join a Flight while they wait for the call to scramble.",
+      voteAverage: 7.412,
+      voteCount: 51,
     }
-    render(<MovieCard movie={noTitle} />);
+    render(<MemoryRouter>
+      <MovieCard movie={noTitle} />
+    </MemoryRouter>);
     const title = screen.getByText("Title not available");
     expect(title).toBeInTheDocument();
   });
@@ -76,9 +101,13 @@ describe("MovieCard component", () => {
       year: null as unknown as string,
       posterPath: "https://image.tmdb.org/t/p/w500/8htJ7keZTwa08aC9OKyiqaq1cNJ.jpg",
       genres: [ "War" ],
-      overview: "A young pilot, fresh out of training, is called to join a Flight while they wait for the call to scramble."
+      overview: "A young pilot, fresh out of training, is called to join a Flight while they wait for the call to scramble.",
+      voteAverage: 7.412,
+      voteCount: 51,
     }
-    render(<MovieCard movie={noYear} />);
+    render(<MemoryRouter>
+      <MovieCard movie={noYear} />
+    </MemoryRouter>);
     const year = screen.getByText(/-/);
     expect(year).toBeInTheDocument();
   });
@@ -90,10 +119,25 @@ describe("MovieCard component", () => {
       year: "2023",
       posterPath: "https://image.tmdb.org/t/p/w500undefined",
       genres: [ "War" ],
-      overview: "A young pilot, fresh out of training, is called to join a Flight while they wait for the call to scramble."
+      overview: "A young pilot, fresh out of training, is called to join a Flight while they wait for the call to scramble.",
+      voteAverage: 7.412,
+      voteCount: 51,
     }
-    render(<MovieCard movie={noMoviePoster} />);
+    render(<MemoryRouter>
+      <MovieCard movie={noMoviePoster} />
+    </MemoryRouter>);
     const image = screen.getByRole("img") as HTMLImageElement;
     expect(image.src).toContain("http://localhost");
+  });
+
+  test("Calls useNavigate when clicking the movie poster or title", () => {
+    render(<MemoryRouter>
+      <MovieCard movie={movie} />
+    </MemoryRouter>);
+    const links = screen.getAllByRole("link");
+    links.forEach((link) => {
+      fireEvent.click(link);
+    })
+    expect(mockedUsedNavigate).toHaveBeenCalledTimes(2);
   });
 });
